4355f5e12d801a33d5806ea5c09b783bb31adef8
Ha Rim Ku <hrku19@gmail.com>
2020-06-18 15:17:55 -0700

    React-Redux form in a modal updated

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,16 +6,12 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
-const isNumber = val => !isNaN(+val);
 
 class CommentForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            rating: '',
-            author: '',
-            text: '',
             touched: {
                 rating: false,
                 author: false,
@@ -23,22 +19,18 @@ class CommentForm extends Component {
             },
             isModalOpen: false
         };
-
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
-    toggleModal() {
+    toggleModal = () => {
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
     }
 
-    handleSubmit(values) {
+    handleSubmit = (values) => {
         console.log("Current state is: " + JSON.stringify(values));
         alert("Current state is: " + JSON.stringify(values));
-
+        
     }
 
     render() {
@@ -50,54 +42,46 @@ class CommentForm extends Component {
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
                         <LocalForm onSubmit={values => this.handleSubmit(values)}>
-                            <Row className="form-group">
-                                <Label htmlFor="rating" md={12}>Rating</Label>
-                                <Col md={12}>
-                                    <Control.select model=".rating" id="rating" name="rating"
-                                        placeholder="Rating"
-                                        className="form-control"  >
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                    </Control.select>
-                                </Col>
-                            </Row>
-                            <Row className="form-group" >
-                                <Label htmlFor="author" md={12}>Your Name</Label>
-                                <Col md={12}>
-                                    <Control.text model=".author" id="author" name="author"
-                                        placeholder="Your Name"
-                                        className="form-control"
-                                        validators={{
-                                            required,
-                                            minLength: minLength(2),
-                                            maxLength: maxLength(15)
-                                        }}
-                                    />
-                                    <Errors 
-                                        className="text-danger"
-                                        model=".author"  //must match the model of corresponding control component
-                                        show="touched"   //show error message only if it's been touched by the user
-                                        component="div"
-                                        messages={{
-                                            required: 'Required',
-                                            minLength: 'Must be at least 2 characters',
-                                            maxLength: 'Must be 15 characters or less'
-                                        }}
-                                    />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="text" md={12}>Comment</Label>
-                                <Col md={12}>
-                                    <Control.textarea model=".text" id="text" name="text"
-                                        rows="6" className="form-control"
-                                    />
-                                </Col>
-                            </Row>
-
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" name="rating"
+                                    className="form-control" >
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group" >
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                    validators={{
+                                        required,
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors 
+                                    className="text-danger"
+                                    model=".author"  //must match the model of corresponding control component
+                                    show="touched"   //show error message only if it's been touched by the user
+                                    component="div"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be at least 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea model=".text" id="text" name="text"
+                                    rows="6" className="form-control"
+                                />
+                            </div>
                             <Button type="submit" value="submit" color="primary">Submit</Button>
                         </LocalForm>
                     </ModalBody>
@@ -132,8 +116,9 @@ function RenderComments({comments}) {
                         {" -- "}{comment.author}{", "}
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                         <br/><br/>
-                    </div>)
+                    </div>)  
                 }
+                <CommentForm />
             </div>
         );
     }
@@ -157,7 +142,6 @@ function CampsiteInfo(props) {
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
-                    <CommentForm />
                 </div>
             </div>
         );
